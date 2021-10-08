e16171e7733f9dd2a05100ee67b3af04b3343e11
arizpando <53623313+arizpando@users.noreply.github.com>
2021-10-08 10:23:09 +0200

    Add files via upload

---
+++ GradosCalculator.py
@@ -0,0 +1,7 @@
+grados=float(input("grados= "))
+gradosolo=int(grados)
+minutos=(grados-gradosolo)*60
+minutosolo=int(minutos)
+segundos=(minutos-minutosolo)*60
+segundosolo=int(segundos)
+print(gradosolo,"·",minutosolo,"'",segundosolo,"''")
